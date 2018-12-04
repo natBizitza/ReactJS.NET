5c1cfe9cdbd150855ebfbede246647189c594359
Natalia Romankevich <romankevich.law@gmail.com>
2018-12-04 12:20:41 +0100

    errors  in the code

--- ReactDemo/Startup.cs
+++ ReactDemo/Startup.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using React.AspNet;
+// Add framework services.
+services.AddMvc();
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+  // If you want to use server-side rendering of React components,
+  // add all the necessary JavaScript files here. This includes
+  // your components as well as all of their dependencies.
+  // See http://reactjs.net/ for more information. Example:
+  //config
+  //    .AddScript("~/Scripts/First.jsx")
+  //    .AddScript("~/Scripts/Second.jsx");
+
+  // If you use an external build too (for example, Babel, Webpack,
+  // Browserify or Gulp), you can improve performance by disabling
+  // ReactJS.NET's version of Babel and loading the pre-transpiled
+  // scripts. Example:
+  //config
+  //    .SetLoadBabel(false)
+  //    .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+});
+app.UseStaticFiles();
+services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+services.AddReact();
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
